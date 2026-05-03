c449b3880ad5eef2d1fe7266c66d9274230929c0
surekis <ejotautas2003@gmail.com>
2026-05-03 18:15:47 +0200

    Initial commit

---
+++ src/test/java/com/example/springproject_authentification/SpringProjectAuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproject_authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectAuthentificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
